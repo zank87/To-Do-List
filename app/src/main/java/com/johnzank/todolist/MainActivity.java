cce046ec64389081d5b28571f17dbae442423698
John Zank <john.k.zank@gmail.com>
2015-09-10 21:34:54 -0400

    Added context menu xml file and logic in MainActivity to utilize menu.

--- app/src/main/java/com/johnzank/todolist/MainActivity.java
+++ app/src/main/java/com/johnzank/todolist/MainActivity.java
@@ -2,10 +2,12 @@ package com.johnzank.todolist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -25,6 +27,7 @@ public class MainActivity extends Activity {
         aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, todoItems);
         ListView mListView = (ListView) findViewById(R.id.list_view);
         mListView.setAdapter(aa);
+        registerForContextMenu(mListView);
 
         final EditText mEditText = (EditText) findViewById(R.id.add_item_text);
         mEditText.setOnKeyListener(new View.OnKeyListener() {
@@ -43,10 +46,28 @@ public class MainActivity extends Activity {
     }
 
     @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int position = info.position;
+        switch (item.getItemId()) {
+            case R.id.menu_item_delete:
+                todoItems.remove(position);
+                aa.notifyDataSetChanged();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.list_item_context, menu);
     }
 
     @Override
