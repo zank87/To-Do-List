d7db330952472920b0019bd0395e2c174dbbcecf
John Zank <john.k.zank@gmail.com>
2015-09-10 21:54:32 -0400

    Created functionality to read and write to a json file in order to preserve the list while a user leaves the app.

--- app/src/main/java/com/johnzank/todolist/MainActivity.java
+++ app/src/main/java/com/johnzank/todolist/MainActivity.java
@@ -1,7 +1,9 @@
 package com.johnzank.todolist;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -12,18 +14,65 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
     private ArrayList<String> todoItems = new ArrayList<>();
     private ArrayAdapter<String> aa;
+    private String mFilename = "testFile.json";
+
+    private void saveToDoItems() {
+        JSONArray array = new JSONArray(todoItems);
+        Writer writer;
+        try {
+            OutputStream out = openFileOutput(mFilename, Context.MODE_PRIVATE);
+            writer = new OutputStreamWriter(out);
+            writer.write(array.toString());
+            writer.close();
+        } catch(IOException e ) {
+            Log.d("Writer", "IO exception: ", e);
+        }
+    }
+
+    private void readToDoItems() {
+        BufferedReader reader;
+        try {
+            InputStream in = openFileInput(mFilename);
+            reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder jsonString = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonString.append(line);
+            }
+            JSONArray array = (JSONArray) new JSONTokener(jsonString.toString()).nextValue();
+            for(int i = 0; i < array.length(); i++)
+                todoItems.add(array.getString(i));
+            reader.close();
+        } catch (IOException e) {
+            Log.d("Reader", "IO exception: ", e);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        readToDoItems();
         aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, todoItems);
         ListView mListView = (ListView) findViewById(R.id.list_view);
         mListView.setAdapter(aa);
@@ -43,6 +92,12 @@ public class MainActivity extends Activity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        saveToDoItems();
     }
 
     @Override
