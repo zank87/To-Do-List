d87cfc81d7b05ecf017582eb7e51d2b61003f4f9
John Zank <john.k.zank@gmail.com>
2015-09-10 20:08:41 -0400

    Created layout. Initialized basic items in app and built basic logic to add items to ArrayList.

--- app/src/main/java/com/johnzank/todolist/MainActivity.java
+++ app/src/main/java/com/johnzank/todolist/MainActivity.java
@@ -2,15 +2,44 @@ package com.johnzank.todolist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+
+    private ArrayList<String> todoItems = new ArrayList<>();
+    private ArrayAdapter<String> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, todoItems);
+        ListView mListView = (ListView) findViewById(R.id.list_view);
+        mListView.setAdapter(aa);
+
+        final EditText mEditText = (EditText) findViewById(R.id.add_item_text);
+        mEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction() == KeyEvent.ACTION_DOWN)
+                    if((keyCode == KeyEvent.KEYCODE_DPAD_CENTER) || (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                        todoItems.add(0, mEditText.getText().toString());
+                        aa.notifyDataSetChanged();
+                        mEditText.setText("");
+                        return true;
+                    }
+                return false;
+            }
+        });
     }
 
     @Override
